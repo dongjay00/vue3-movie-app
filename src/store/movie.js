e1dba0e3ef72f793b8f1f85c837c7da654234b95
dongjay00 <cjswodlehdgn@gmail.com>
2021-08-22 16:03:40 +0900

    remove duplicated ID

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import _uniqBy from "lodash/uniqBy";
 
 export default {
   namespaced: true,
@@ -28,7 +29,7 @@ export default {
       );
       const { Search, totalResults } = res.data;
       commit("updateState", {
-        movies: Search,
+        movies: _uniqBy(Search, "imdbID"),
       });
       console.log(totalResults);
       console.log(typeof totalResults);
@@ -44,7 +45,7 @@ export default {
           );
           const { Search } = res.data;
           commit("updateState", {
-            movies: [...state.movies, ...Search],
+            movies: [...state.movies, ..._uniqBy(Search, "imdbID")],
           });
         }
       }
