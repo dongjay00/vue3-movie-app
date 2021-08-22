a9b71bf90e04ff627daa88d948a5f31595fc3876
dongjay00 <cjswodlehdgn@gmail.com>
2021-08-22 15:48:16 +0900

    supplement searching feature

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,7 +19,7 @@ export default {
     },
   },
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload;
       const OMDB_API_KEY = "7035c60c";
 
@@ -30,6 +30,24 @@ export default {
       commit("updateState", {
         movies: Search,
       });
+      console.log(totalResults);
+      console.log(typeof totalResults);
+
+      const total = parseInt(totalResults, 10);
+      const pageLength = Math.ceil(total / 10);
+
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > number / 10) break;
+          const res = await axios.get(
+            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+          );
+          const { Search } = res.data;
+          commit("updateState", {
+            movies: [...state.movies, ...Search],
+          });
+        }
+      }
     },
   },
 };
