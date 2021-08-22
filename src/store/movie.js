f55c7860c70075cdfd8557dff2334cf92c8e4cda
dongjay00 <cjswodlehdgn@gmail.com>
2021-08-22 15:08:25 +0900

    add searching movie

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,25 +1,35 @@
+import axios from "axios";
+
 export default {
-  // module!
   namespaced: true,
-  // data!
   state: () => ({
     movies: [],
+    message: "",
+    loading: false,
   }),
-  // computed!
-  getters: {
-    movieIds(state) {
-      return state.movies.map((m) => m.imdbID);
-    },
-  },
-  // methods!
-  // 변이
+  getters: {},
   mutations: {
+    updateState(state, payload) {
+      Object.keys(payload).forEach((key) => {
+        state[key] = payload[key];
+      });
+    },
     resetMovies(state) {
       state.movies = [];
     },
   },
-  // 비동기
   actions: {
-    searchMovies() {},
+    async searchMovies({ commit }, payload) {
+      const { title, type, number, year } = payload;
+      const OMDB_API_KEY = "7035c60c";
+
+      const res = await axios.get(
+        `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
+      );
+      const { Search, totalResults } = res.data;
+      commit("updateState", {
+        movies: Search,
+      });
+    },
   },
 };
