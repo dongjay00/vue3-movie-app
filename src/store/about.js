f8cb08022707319159226fc74cd85efb8dc37042
dongjay00 <cjswodlehdgn@gmail.com>
2021-08-23 21:40:24 +0900

    add about page

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: "Jay Lee",
+    email: "cjswodlehdgn@gmail.com",
+    blog: "https://github.com/dongjay00",
+    phone: "+82-10-1234-5678",
+    image:
+      "https://user-images.githubusercontent.com/68373235/130445569-b463b201-02a1-431e-8f2b-42c1739f7e4f.jpg",
+  }),
+};
